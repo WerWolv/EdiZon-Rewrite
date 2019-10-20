892f293a47fa961fed4ce4b68ad7e575eab80b8a
WerWolv <werwolv98@gmail.com>
2019-10-20 02:25:10 +0200

    Common header restructuring

--- common/include/edizon.hpp
+++ common/include/edizon.hpp
@@ -20,47 +20,49 @@
 #pragma once
 
 #include <switch.h>                 // libnx
-
-#ifndef __SYSMODULE__
-    #include <borealis/logger.hpp>      // Borealis Logger
-    using Logger = brls::Logger;
-    
-    #include "helpers/lang_entry.hpp"   // Translation helper
-#endif
-
 #include "edz_types.hpp"            // Types associated with EdiZon
-
 #include "helpers/macros.hpp"       // Helper macros
 
-using namespace std::string_literals;   // String user defined literals
 
-/* || EDIZON COMPILE TIME SETTINGS || */
+#ifndef __SYSMODULE__                           // EdiZon Homebrew context
 
-/* The EdiZon paths on the SD card */
-#define EDIZON_BASE_DIR                 "sdmc:/switch/EdiZon"
-    #define EDIZON_BACKUP_DIR               EDIZON_BASE_DIR "/backups"
-    #define EDIZON_BATCH_BACKUP_DIR         EDIZON_BASE_DIR "/batch_backups"
-    #define EDIZON_COMMON_RESTORE_DIR       EDIZON_BASE_DIR "/restore"
-    #define EDIZON_CONFIGS_DIR              EDIZON_BASE_DIR "/configs"
-    #define EDIZON_SCRIPTS_DIR              EDIZON_BASE_DIR "/scripts"
-    #define EDIZON_LIBS_DIR                 EDIZON_BASE_DIR "/scripts/libs"
-    #define EDIZON_CHEATS_DIR               EDIZON_BASE_DIR "/cheats"
-    #define EDIZON_TMP_DIR                  EDIZON_BASE_DIR "/tmp"
+    #include <borealis/logger.hpp>              // Borealis Logger
+    using Logger = brls::Logger;
+    
+    #include "helpers/lang_entry.hpp"           // Translation helper
+    using namespace std::string_literals;       // String user defined literals
 
-#define BACKUP_FILE_EXTENSION           ".edz"
+    /* || EDIZON COMPILE TIME SETTINGS || */
 
-/* Whether the splash screen should be displayed on launch */
-#define SPLASH_ENABLED              true
+    /* The EdiZon paths on the SD card */
+    #define EDIZON_BASE_DIR                 "sdmc:/switch/EdiZon"
+        #define EDIZON_BACKUP_DIR               EDIZON_BASE_DIR "/backups"
+        #define EDIZON_BATCH_BACKUP_DIR         EDIZON_BASE_DIR "/batch_backups"
+        #define EDIZON_COMMON_RESTORE_DIR       EDIZON_BASE_DIR "/restore"
+        #define EDIZON_CONFIGS_DIR              EDIZON_BASE_DIR "/configs"
+        #define EDIZON_SCRIPTS_DIR              EDIZON_BASE_DIR "/scripts"
+        #define EDIZON_LIBS_DIR                 EDIZON_BASE_DIR "/scripts/libs"
+        #define EDIZON_CHEATS_DIR               EDIZON_BASE_DIR "/cheats"
+        #define EDIZON_TMP_DIR                  EDIZON_BASE_DIR "/tmp"
 
-/* Current web API version */
-#define EDIZON_API_VERSION          "v4"
-#define SWITCHCHEATSDB_API_VERSION  "v1"
+    #define BACKUP_FILE_EXTENSION           ".edz"
 
-/* Web API URLs */
-#define EDIZON_API_URL              "http://api.edizon.werwolv.net/" EDIZON_API_VERSION 
-#define SWITCHCHEATSDB_API_URL      "https://www.switchcheatsdb.com/api/" SWITCHCHEATSDB_API_VERSION
-#define ANONFILE_API_URL            "https://api.anonfile.com"
+    /* Whether the splash screen should be displayed on launch */
+    #define SPLASH_ENABLED              true
 
-/* Debugging */
-#define DEBUG_MODE_ENABLED          true
+    /* Current web API version */
+    #define EDIZON_API_VERSION          "v4"
+    #define SWITCHCHEATSDB_API_VERSION  "v1"
 
+    /* Web API URLs */
+    #define EDIZON_API_URL              "http://werwolv.net:1337/" EDIZON_API_VERSION 
+    #define SWITCHCHEATSDB_API_URL      "https://www.switchcheatsdb.com/api/" SWITCHCHEATSDB_API_VERSION
+    #define ANONFILE_API_URL            "https://api.anonfile.com"
+
+    /* Debugging */
+    #define DEBUG_MODE_ENABLED          true
+
+#else               // EdiZon Sysmodule context
+
+
+#endif
