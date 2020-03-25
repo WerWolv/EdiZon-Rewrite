4788f4ddce7669971ac2169bf8e6089d97793bbc
WerWolv <werwolv98@gmail.com>
2020-03-25 14:05:27 +0100

    Companion stuff

---
+++ companion/src/main/java/net/werwolv/edizon/ui/GuiMain.java
@@ -0,0 +1,24 @@
+package net.werwolv.edizon.ui;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class GuiMain extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Scene scene = new Scene(new StackPane(l), 640, 480);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
